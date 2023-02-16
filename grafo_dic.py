6af0cff8a4539a02fedf0e8f55140a62ffc73469
Abraham Rmz <abrahammoreno044@gmail.com>
2023-02-16 13:17:33 -0600

    Estructura de grafo mejorada, falta djikstra

--- grafo_dic.py
+++ grafo_dic.py
@@ -1,4 +1,3 @@
-import enum
 from typing import (List, Iterator, FrozenSet, Set, Tuple )
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -10,7 +9,7 @@ tipo_vecinos = FrozenSet[tipo_nodo]
 
 class Arista:
     
-    def __init__(self, vecinos: Tuple[tipo_nodo, tipo_nodo], peso=0):
+    def __init__(self, vecinos: Tuple[tipo_nodo, tipo_nodo], peso: float=0):
         if len(vecinos) != 2:
             raise Exception("Arista no valida")
         self.vecinos: tuple[tipo_nodo, tipo_nodo] = vecinos
@@ -34,40 +33,17 @@ class Arista:
     def arista_nx(self)-> Tuple[tipo_nodo, tipo_nodo, dict]:
         return (self.vecinos[0], self.vecinos[1], {"weight": self.peso})
 
-
-#Por simplicidad hice que dos vecinos son iguales sin importar su peso
-class Vecino:
-
-    def __init__(self, vecino: tipo_nodo, peso: float = 0):
-        self.nodo_vecino: tipo_nodo = vecino
-        self.peso = peso
-
-    def __repr__(self):
-        return f"[{self.nodo_vecino} <{self.peso}>]"
-
-    #Ignora peso
-    def __eq__(self, o):
-        return isinstance(o, Vecino)\
-            and self.nodo_vecino == o.nodo_vecino
-    
-    #Ignora peso
-    def __hash__(self):
-        return hash(self.nodo_vecino)
-
-
 # Función que agrega valor a set de diccionario si existe, lo crea si no
-# Retorna 1 si se agregó un valor
-def aumentar_set_dict(diccionario: dict, llave, valor):
-    if llave in diccionario:
-        diccionario[llave].add(valor)
+def aumentar_dict_dict(diccionario: dict[tipo_nodo, dict[tipo_nodo, float]], nodo1, nodo2, peso):
+    if nodo1 in diccionario:
+        diccionario[nodo1][nodo2] = peso
     else:
-        diccionario[llave] = set([valor])
-
+        diccionario[nodo1] = {nodo2: peso}
 
 class Grafo:
 
     def __init__(self, adyacencias: set[Arista]):
-        self.adyacencias: dict[tipo_nodo, set[Vecino]] = {}
+        self.adyacencias: dict[tipo_nodo, dict[tipo_nodo,float]] = {}
         for arista in adyacencias:
             self.agregar_arista(arista)
 
@@ -75,30 +51,29 @@ class Grafo:
     def agregar_arista(self, arista: Arista):
         for i, nodo in enumerate(arista.vecinos):
             nodo_vecino = arista.vecinos[1 - i]
-            aumentar_set_dict(self.adyacencias, nodo,
-                                      Vecino(nodo_vecino, arista.peso))
+            aumentar_dict_dict(self.adyacencias, nodo, nodo_vecino, arista.peso)
 
     # Elimina la arista con el par de vecinos dados sin importar el peso
     def eliminar_arista(self, arista: Arista):
         for i, nodo in enumerate(arista.vecinos):
             nodo_vecino = arista.vecinos[1 - i]
-            self.adyacencias[nodo].remove(Vecino(nodo_vecino))
+            del self.adyacencias[nodo][nodo_vecino]
             if nodo_vecino == nodo:
                 break
 
     def get_nodos(self):
         return self.adyacencias.keys()
 
+    def get_vecinos(self, nodo: tipo_nodo) -> FrozenSet[tipo_nodo]:
+        return frozenset(self.adyacencias[nodo].keys())
+
     def get_aristas(self) -> FrozenSet[Arista]:
         set_aristas: set[Arista] = set()
         for nodo in self.get_nodos():
             for vecino in self.get_vecinos(nodo):
-                set_aristas.add(Arista((nodo, vecino.nodo_vecino),vecino.peso))
+                set_aristas.add(Arista((nodo, vecino), self.adyacencias[nodo][vecino]))
         return frozenset(set_aristas)
-
-    def get_vecinos(self, nodo: tipo_nodo) -> FrozenSet[Vecino]:
-        return frozenset(self.adyacencias[nodo])
-
+    
     def __str__(self):
         salida = ""
         for nodo in migrafo.adyacencias.keys():
@@ -116,10 +91,10 @@ class Grafo:
         nx.draw_networkx_edge_labels(grafo_nx, pos, edge_labels=labels)
         plt.show()
 
-    def djikstra(self, nodo_inicial: tipo_nodo) -> dict[tipo_nodo, float]:
-        distancias_minimas: dict[tipo_nodo,float] = {}
-        posibles_rutas = dict[tipo_nodo,float] = {}
-        return distancias_minimas
+    # def djikstra(self, nodo_inicial: tipo_nodo) -> dict[tipo_nodo, float]:
+    #     distancias_minimas: dict[tipo_nodo,float] = {}
+    #     posibles_rutas: dict[tipo_nodo,float] = {nodo:peso for }
+    #     return distancias_minimas
 
 aristas = set([
     Arista(('A', 'B'), 1),
