5e327fa9b8e89741c4ebf3f2e4c578938bb20c39
Abraham Rmz <abrahammoreno044@gmail.com>
2023-02-28 15:45:44 -0600

    Agregando archivos para tarea 3

---
+++ T4_Busqueda_Grafo/busquedas_no_informadas.py
@@ -0,0 +1,9 @@
+from grafo import Grafo
+
+# AQUI PON EL CÓDIGO PARA TU BUSQUEDA NO INFORMADA
+
+def ruta_mas_corta_profundidad(Grafo: Grafo, Nodo_inicial,Nodo_final):
+    pass
+
+def ruta_mas_corta_amplitud(Grafo: Grafo, Nodo_inicial,Nodo_final):
+    pass
---
+++ T4_Busqueda_Grafo/pruebas.py
@@ -0,0 +1,8 @@
+from grafo import Grafo
+from grafos_ejemplo import aristas_g1,aristas_g2,aristas_g3
+import timeit
+
+# En este archivo se realizarán las pruebas
+
+lista_grafos = [Grafo(aristas_g1),Grafo(aristas_g2),Grafo(aristas_g3)]
+
